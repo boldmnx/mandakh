9c52a65eeeae151fd4525db3bb4e592e667338fa
baynaa <sw18d002@mandakh.edu.mn>
2023-05-03 15:12:34 +0800

    python html get data

---
+++ MUdirtgal/5-3/main.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+a = []
+for i in range(0, 300, 25):
+    url = 'https://www.spoj.com/RGB7/ranks2/?start='+str(i)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    table = soup.find('table', class_='table table-condensed')
+    # print(table)
+
+    for row in table.find_all('tr'):
+        columns = row.find_all('td')
+        if len(columns) > 0:
+            rank = columns[0].text.strip()
+            username = columns[1].text.strip()
+            solved = columns[2].text.strip()
+            # print(rank, username, solved)
+            a.append([rank, username, solved])
+
+print(a)
+
+with open('your_file.txt', 'w') as f:
+    for line in a:
+        f.write(f"{line}\n")
