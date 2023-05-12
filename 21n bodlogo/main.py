695af358a3365f7b1a43db2a85faf88b5f9359c6
batsukh0222 <124018969+batsukh0222@users.noreply.github.com>
2023-05-12 15:04:47 +0800

    bodlogo 3 aa hiiv

---
+++ 21n bodlogo/main.py
@@ -0,0 +1,23 @@
+# 1 Томоос жижигт
+# print("K".lower())
+
+# 2: 4 тооны бага
+# a,b,c,d=map(int,input().split())
+# s=a
+# if(s>b):
+#     s=b
+# if(s>c):
+#     s=c
+# if(s>d):
+#     s=d
+# print(s)
+
+# 3	Цэгийн байрлал
+# x, y= map(int,input().split())
+# if x>0 and y>0:
+#     print("I")
+# elif x<0 and y>0:
+#     print("II")
+# elif x<0 and y<0:
+#     print("III")
+# else: print("IV")
