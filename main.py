9aa644d480d55c8a2eb58a1df00e7fd0f0fb65f1
batsukh0222 <124018969+batsukh0222@users.noreply.github.com>
2023-05-12 16:51:04 +0800

    bodlogo 11

--- main.py
+++ main.py
@@ -21,3 +21,16 @@
 # elif x<0 and y<0:
 #     print("III")
 # else: print("IV")
+
+# 11	Палиндром эсэх	
+# too = str(input())
+# con = True
+# tugsgul = len(too) - 1
+# for ehlel in range(0, len(too)):
+#     if(too[ehlel] != too[tugsgul]):
+#         con = False
+#     tugsgul -= 1
+# if(con):
+#     print("YES")
+# else:
+#     print("NO")
