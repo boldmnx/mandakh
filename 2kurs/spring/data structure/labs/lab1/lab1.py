0b25b66cbcabdb93fdcc03987629fea6a20d5397
jboldoo <jboldoo98@gmail.com>
2024-02-02 15:41:03 +0800

    lab1, lab2 of client and lab1, lab2 of data structure

--- 2kurs/spring/data structure/labs/lab1/lab1.py
+++ 2kurs/spring/data structure/labs/lab1/lab1.py
@@ -1,22 +1,90 @@
-n = map(int, input().split())
 
 
-print([n[0]])
-
-# def maxx(*a):
-#     m = a[0][0]
-#     for i in a[0]:
-#         if m < i:
-#             m = i
-#     return m
+def maxx(*a):
+    m = a[0][0]
+    for i in a[0]:
+        if m < i:
+            m = i
+    return m
 
 
-# def minn(*a):
-#     m = a[0][0]
-#     for i in a[0]:
-#         if m > i:
-#             m = i
-#     return m
+def avg(*a):
+    sum = 0
+    for i in a[0]:
+        sum += i
+    return sum//len(a[0])
 
 
-# print(maxx(n))
+def minn(*a):
+    m = a[0][0]
+    for i in a[0]:
+        if m > i:
+            m = i
+    return m
+
+
+def weekend_method(weekend):
+    if weekend == 1:
+        print('Monday')
+    elif weekend == 2:
+        print('Tuesday')
+    elif weekend == 3:
+        print('Wednesday')
+    elif weekend == 4:
+        print('thursday')
+    elif weekend == 5:
+        print('Friday')
+    elif weekend == 6:
+        print('Saturday')
+    elif weekend == 7:
+        print('Sunday')
+    else:
+        print('1-7 hoorond too oruulna uu')
+
+
+# Daalgavar 1
+
+# w = int(input())
+# weekend_method(w)
+
+# Daalgavar 2
+
+# n = list(map(int, input().split()))
+
+# print(f'max: {maxx(n)}')
+# print(f'avg: {avg(n)}')
+# print(f'min: {minn(n)}')
+
+# Daalgavar 3
+
+
+class Phone:
+    def __init__(self, ner):
+        self.ner = ner
+
+    def __str__(self):
+        print(f'Tuunii ner: {self.ner}')
+
+
+class Homephone(Phone):
+    def __init__(self, ner, cable):
+        Phone.__init__(self, ner)
+        self.cable = cable
+
+    def __str__(self):
+        print(f'Каблын төрөл: {self.cable}')
+
+
+class Cellphone(Phone):
+    def __init__(self, ner, touch=False):
+        Phone.__init__(self, ner)
+        self.touch = touch
+
+    def __str__(self):
+        print(f'Утасны нэр: {self.touch}')
+
+
+iphone = Cellphone('iphone 11', True)
+landline = Homephone('landline', 'RJ-11')
+iphone.__str__()
+landline.__str__()
---
+++ 2kurs/spring/data structure/labs/lab2/lab2.py
@@ -0,0 +1,49 @@
+class Stack:
+    def __init__(self):
+        self.__list1 = []
+
+    def push(self, e):
+        self.__list1 += [e]
+
+    def pop(self):
+        if self.is_empty():
+            return 'hooson bna'
+        self.__list1 = self.__list1[:-1]
+
+    def top(self):
+        if self.is_empty() == False:
+            return self.__list1[-1]
+        return 'hooson bna'
+
+    def bottom(self):
+        if self.lenn() != 0:
+            return self.__list1[0]
+        return 'hooson bna'
+
+    def lenn(self):
+        count = 0
+        for i in self.__list1:
+            count += 1
+        return count
+
+    def is_empty(self):
+        if self.lenn() == 0:
+            return True
+        return False
+
+    def getItems(self):
+        return self.__list1
+
+
+s = Stack()
+s.push('A')
+s.push('b')
+s.push('C')
+print(s.getItems())
+print(f'pop: {s.pop()}')
+
+print(f'top: {s.top()}')
+print(f'bot: {s.bottom()}')
+
+print(f'lenn: {s.lenn()}')
+print(f'isEmpty: {s.is_empty()}')
