b481d5dc0aa10508e44ace16f0fffac6b1732fb9
Bold-Erdene Jigmed <77866161+boldoo-dev@users.noreply.github.com>
2024-12-10 21:26:55 +0800

    lab15-data

--- 3kurs/namar/system/lab15/backend/app/urls.py
+++
@@ -1,8 +0,0 @@
-
-from app import views
-from django.urls import path
-from .views import StudentGetCreate, StudentUpdateDelete
-urlpatterns = [
-    path('students/', StudentGetCreate.as_view()),
-    path('students/<int:pk>/', StudentUpdateDelete.as_view()),
-]
