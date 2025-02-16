d3829691c2bb386dcf9a36e8e86ba9a4160da31c
boldoo-code <jboldoo98@gmail.com>
2025-02-16 19:44:49 +0800

    bug fix

--- 3kurs/namar/ipro/biy daalt/product_app/product_pro/product_pro/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path, include
-from product_app import authView, views
-
-
-urlpatterns = [
-    path('api/auth/', authView.authCheckService),
-    path('api/product/', views.productCheckService),
-]
